6147fe48936a8563391728e1d5695e5e008107db
rawdhadaikhi <raoudhadayki@gmail.com>
2021-01-15 11:39:12 +0100

    first commit checkpoint1 reactjs

--- src/App.js
+++ src/App.js
@@ -1,22 +1,38 @@
-import logo from './logo.svg';
+import React from 'react';
+import image from './assets/challenge.jpg';
 import './App.css';
+import './sign.js';
+
+import 'bootstrap/dist/css/bootstrap.min.css'
+import {Button,Alert,Container,Row,Col,Form,Image} from 'react-bootstrap'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <Container fluid>
+        <Row>
+          <Col>
+           <Image src= {image} className ="rounded float-left" style={{width:700,height:500}}></Image>
+          </Col>
+          <Col style={{padding:50}}>
+          <Form>
+           <Form.Group>
+           <Alert style={{color: 'orange'}}>Join our community and start your challenge</Alert>
+            <Form.Label>Email Address :</Form.Label>
+            <Form.Control type="email" placeholder="exemple@email.com"></Form.Control>
+            <Form.Text>we'll never share your email address, trust us!</Form.Text>
+            <Form.Label>Password:</Form.Label>
+            <Form.Control type="password" placeholder="password"></Form.Control>
+           </Form.Group>
+           <Button className="btn btn-warning" type="submit">SIGN IN</Button>
+           <Alert>New to START ?</Alert>
+           <a href="#" onClick='sign.js'>Sign up now</a>
+
+          </Form>
+          </Col>
+        </Row>
+      </Container>
       </header>
     </div>
   );
---
+++ src/sign.js
@@ -0,0 +1,33 @@
+/*import {Form,Select,Button,Checkbox,Option} from "react-bootstrap";
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+function signup() {
+    return(
+     <Form>
+         <Form.Group>
+           <Form.Text>Sign Up</Form.Text> 
+           <Form.Label>First Name:</Form.Label> 
+           <Form.Control type="text" placeholder="your name"></Form.Control>
+           <Form.Label>Last Name:</Form.Label> 
+           <Form.Control type="text" placeholder="your family name"></Form.Control>
+           <Form.Label>Age:</Form.Label> 
+           <Form.Control type="number" placeholder="your age"></Form.Control>
+           <Form.Label>Email Address:</Form.Label> 
+           <Form.Control type="email" placeholder="exemple@email.com"></Form.Control>
+           <Form.Label>Password:</Form.Label> 
+           <Form.Control type="password" placeholder="password"></Form.Control>
+           <Form.Label>Confirm Password:</Form.Label> 
+           <Form.Control type="password" placeholder="password"></Form.Control>
+           <Form.Label>choose Level:</Form.Label> 
+           <Select class="form-control" id="exampleFormControlSelect1">
+           <Option>easy</Option>
+           <Option>medium</Option>
+           <Option>hard</Option>
+           </Select>
+           <Checkbox class="form-check-input" type="checkbox" >I'm not a robot !</Checkbox>
+           <Button className="btn btn-warning" type="submit">SIGN UP</Button>
+         </Form.Group>
+     </Form>
+    );
+    
+}*/
